6919a70adf53f90c295d054958697da45af89dc0
Audu Billy <audubilly89@gmail.com>
2021-02-10 19:43:52 +0100

    fix

--- Tests/BankingApplication/BankTest.java
+++ Tests/BankingApplication/BankTest.java
@@ -26,56 +26,51 @@ class BankTest {
 
 
     @Test
-    void testThatABankCanHaveCustomers(){
-        customer = new Customer("billy","audu","adams","semicolon,Yaba",
-                "billyaudu@gmail.com","09056790444" );
-        bank.setCustomers(customer,bankAccount);
-        assertEquals(customer,bank.getCustomers()[0]);
+    void testThatABankCanHaveCustomers() {
+        customer = new Customer("billy", "audu", "adams", "semicolon,Yaba",
+                "billyaudu@gmail.com", "09056790444");
+        bank.setCustomers(customer, bankAccount);
+        assertEquals(customer, bank.getCustomers()[0]);
     }
 
     @Test
-    void testThatTwoCustomersAreNotAssignedTheSameSpot(){
-        customer = new Customer("billy","audu","adams","semicolon,Yaba",
-                "billyaudu@gmail.com","09056790444" );
-        bankAccount = new BankAccount(1,1000);
+    void testThatTwoCustomersAreNotAssignedTheSameSpot() {
+        customer = new Customer("billy", "audu", "adams", "semicolon,Yaba",
+                "billyaudu@gmail.com", "09056790444");
+        bankAccount = new BankAccount(1, 1000);
         bank.setCustomers(customer, bankAccount);
 //        System.out.println(customer.toString() + bankAccount.toString());
 
-      Customer customer2 = new Customer("bil","aud","adams","semicolon,Yaba",
-                "billyaudu@gmail.com","09056790444" );
-       BankAccount bankAccount2 = new BankAccount(2,1000);
-        bank.setCustomers(customer2,bankAccount2);
+        Customer customer2 = new Customer("bil", "aud", "adams", "semicolon,Yaba",
+                "billyaudu@gmail.com", "09056790444");
+        BankAccount bankAccount2 = new BankAccount(2, 1000);
+        bank.setCustomers(customer2, bankAccount2);
 
 
 //        assertEquals(customer,bank.getCustomers()[0]);
 //        assertEquals(customer2, bank.getCustomers()[1]);
     }
+
     @Test
-    void testThatTransactionsArePossible(){
-        customer = new Customer("billy","audu","adams","semicolon,Yaba",
-                "billyaudu@gmail.com","09056790444" );
-        bankAccount = new BankAccount(1,1000);
+    void testThatTransactionsArePossible() {
+        customer = new Customer("billy", "audu", "adams", "semicolon,Yaba",
+                "billyaudu@gmail.com", "09056790444");
+        bankAccount = new BankAccount(1, 1000);
         bank.setCustomers(customer, bankAccount);
 
-        Customer customer2 = new Customer("bil","aud","adams","semicolon,Yaba",
-                "billyaudu@gmail.com","09056790444" );
-        BankAccount bankAccount2 = new BankAccount(2,1000);
-        bank.setCustomers(customer2,bankAccount2);
+        Customer customer2 = new Customer("bil", "aud", "adams", "semicolon,Yaba",
+                "billyaudu@gmail.com", "09056790444");
+        BankAccount bankAccount2 = new BankAccount(2, 1000);
+        bank.setCustomers(customer2, bankAccount2);
 
 //        System.out.println(bank.withdrawal(bankAccount,2000));
 //        assertEquals(1000,bankAccount.getBalance());
 //        System.out.println(bank.withdrawal(bankAccount,1000));
 //        assertEquals(0,bankAccount.getBalance());
 //
-        System.out.println( bank.depositMoney(bankAccount2,-100));
-       assertEquals(1000,bankAccount2.getBalance());
-//
-//        System.out.println(bank.transfer(bankAccount2,bankAccount,200));
-//       assertEquals(1200,bankAccount.getBalance());
-//        System.out.println(bankAccount.toString());
-
-//       assertEquals(900,bankAccount2.getBalance());
-//        System.out.println(bankAccount2.toString());
+        System.out.println(bank.depositMoney(bankAccount2, -100));
+        assertEquals(1000, bankAccount2.getBalance());
+        // ToDO
 //
 
     }
